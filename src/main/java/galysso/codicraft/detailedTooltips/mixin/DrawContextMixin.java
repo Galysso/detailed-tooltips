ad265085a4aed0f4101836f51799387c384ea51c
Galysso <170312039+Galysso@users.noreply.github.com>
2025-04-17 12:30:06 +0200

    Fixed conflict with Legendary Tooltips

--- src/main/java/galysso/codicraft/detailedTooltips/mixin/DrawContextMixin.java
+++ src/main/java/galysso/codicraft/detailedTooltips/mixin/DrawContextMixin.java
@@ -4,7 +4,6 @@ import com.anthonyhilyard.iceberg.component.IExtendedText;
 import com.anthonyhilyard.iceberg.component.TitleBreakComponent;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.tooltip.HoveredTooltipPositioner;
 import net.minecraft.client.gui.tooltip.TooltipComponent;
 import net.minecraft.client.gui.tooltip.TooltipPositioner;
 import net.minecraft.item.tooltip.TooltipData;
@@ -12,9 +11,10 @@ import net.minecraft.text.OrderedText;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.ArrayList;
@@ -26,10 +26,24 @@ public class DrawContextMixin {
     @Shadow
     private void drawTooltip(TextRenderer textRenderer, List<TooltipComponent> components, int x, int y, TooltipPositioner positioner) {}
 
+    public List<Text> capturedTextList;
+
     @Inject(method = "drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;Ljava/util/Optional;II)V", at = @At("HEAD"), cancellable = true)
     public void drawTooltip(TextRenderer textRenderer, List<Text> text, Optional<TooltipData> data, int x, int y, CallbackInfo ci) {
+        this.capturedTextList = text;
+    }
+
+    @ModifyArg(
+        method = "drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;Ljava/util/Optional;II)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/DrawContext;drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;IILnet/minecraft/client/gui/tooltip/TooltipPositioner;)V"
+        ),
+        index = 1
+    )
+    private List<TooltipComponent> modifyTooltipList(List<TooltipComponent> value) {
         List<TooltipComponent> list = new ArrayList<>();
-        for (Text t : text) {
+        for (Text t : capturedTextList) {
             if (t.getString().startsWith("[SECTION]")) {
                 Text newString = Text.literal(t.getString().substring(9));
                 OrderedText orderedText = newString.asOrderedText();
@@ -43,8 +57,6 @@ public class DrawContextMixin {
                 list.add(component);
             }
         }
-        data.ifPresent((datax) -> list.add(list.isEmpty() ? 0 : 1, TooltipComponent.of(datax)));
-        drawTooltip(textRenderer, list, x, y, HoveredTooltipPositioner.INSTANCE);
-        ci.cancel();
+        return list;
     }
 }
